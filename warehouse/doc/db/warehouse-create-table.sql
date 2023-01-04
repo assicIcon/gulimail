faeaad216718c6c63787fb4aafc4d32a1df275c6
assicIcon <429725883@qq.com>
2023-01-04 16:28:30 +0800

    application properties

--- warehouse/doc/db/warehouse-create-table.sql
+++ warehouse/doc/db/warehouse-create-table.sql
@@ -64,14 +64,58 @@ create index idx_sku_id on warehouse_stock_detail (sku_id) comment '产品SKU_ID
 create index idx_relation_id on warehouse_stock_detail (relation_id) comment '关联ID索引';
 create index idx_operator_id on warehouse_stock_detail (operator_id) comment '操作人ID索引';
 
-#
+# 供应商
+create table supplier
+(
+    id            bigint unsigned auto_increment not null comment '自增ID' primary key,
+    supplier_id   varchar(32) unique             not null comment '供应商ID',
+    supplier_name varchar(64)                    not null comment '供应商名称',
+    level         tinyint(4)                     not null comment '等级',
+    status        tinyint(4) default 0           not null comment '状态',
+    description   varchar(256)                   null comment '描述',
+    create_time   datetime                       not null comment '创建时间',
+    update_time   datetime                       not null comment '修改时间'
+) comment '供应商';
+
+# 供应商联系人
+create table supplier_contact
+(
+    id            bigint unsigned auto_increment not null comment '自增ID' primary key,
+    supplier_id   varchar(32)                    not null comment '供应商ID',
+    contract_name varchar(64)                    not null comment '联系人名称',
+    phone_number  varchar(64)                    not null comment '联系人手机号码',
+    sort          int default 0                  not null comment '排序',
+    create_time   datetime                       not null comment '创建时间',
+    update_time   datetime                       not null comment '修改时间'
+) comment '供应商联系人';
+create index idx_supplier_id on supplier_contact (supplier_id) comment '供应商ID索引';
+
+# 采购单
 create table purchase_order
 (
-    id          bigint unsigned auto_increment not null comment '自增ID' primary key,
-    purchase_id varchar(32) unique             not null comment '采购ID',
-    operator_id varchar(32)                    not null comment '操作人ID',
-    status      tinyint(4) default 0           not null comment '状态',
-    create_time datetime                       not null comment '创建时间',
-    update_time datetime                       not null comment '修改时间'
-)
+    id            bigint unsigned auto_increment not null comment '自增ID' primary key,
+    purchase_id   varchar(32) unique             not null comment '采购ID',
+    supplier_id   varchar(32)                    not null comment '供应商ID',
+    creator_id    varchar(32)                    not null comment '创建人ID',
+    status        tinyint(4) default 0           not null comment '状态',
+    total_amount  decimal(10, 2)                 not null comment '总金额',
+    purchase_time datetime                       null comment '采购时间',
+    arrival_time  datetime                       null comment '到货时间',
+    remark        varchar(256)                   null comment '备注',
+    create_time   datetime                       not null comment '创建时间',
+    update_time   datetime                       not null comment '修改时间'
+) comment '采购单';
 
+# 采购单明细
+create table purchase_order_item
+(
+    id            bigint unsigned auto_increment not null comment '自增ID' primary key,
+    purchase_id   varchar(32)                    not null comment '采购单ID',
+    sku_id        varchar(32)                    not null comment '产品SKU_ID',
+    price         decimal(10, 2)                 not null comment '单价',
+    count         int                            not null comment '数量',
+    arrival_count int default 0                  not null comment '到货数量',
+    create_time   datetime                       not null comment '创建时间',
+    update_time   datetime                       not null comment '修改时间'
+) comment '采购单明细';
+create index idx_purchase_id
